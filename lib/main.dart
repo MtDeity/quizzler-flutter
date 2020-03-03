435c738092be31ff4bf73f68530c1c2c800d77bd
MtDeity <o8olo853q43@gmail.com>
2020-03-03 13:37:04 +0900

    create a QuizBrain class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/question.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBtain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -39,14 +41,6 @@ class _QuizPageState extends State<QuizPage> {
 
   int questionNumber = 0;
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -59,7 +53,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber % 3].questionText,
+                quizBtain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -84,7 +78,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 bool correctAnswer =
-                    questionBank[questionNumber % 3].questionAnswer;
+                    quizBtain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == true) {
                   print('Correct!');
                 } else {
@@ -112,7 +106,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 bool correctAnswer =
-                    questionBank[questionNumber % 3].questionAnswer;
+                    quizBtain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == false) {
                   print('Correct!');
                 } else {
